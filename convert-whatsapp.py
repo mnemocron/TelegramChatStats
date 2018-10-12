580e04b154dee3db9ccfa0fa22a1108f0db488ae
Simon M. Burkhardt <simon.burkhardt.prime@gmail.com>
2018-10-12 11:44:28 +0200

    fixed issue 1 - whatsapp date format

--- convert-whatsapp.py
+++ convert-whatsapp.py
@@ -29,6 +29,9 @@ parser = optparse.OptionParser('convert-whatsapp.py')
 parser.add_option('-i', '--input-file', 	dest='indir', 	type='string', 	help='chat history file')
 (opts, args) = parser.parse_args()
 
+dateformat = 'EU'
+dateseprarator = '.'
+
 # Writes a dict in json format to a file
 def dump_to_json_file(filename, data):
 	with open(filename, 'w', encoding='utf-8') as fh:
@@ -40,9 +43,18 @@ def load_file_to_srting(filename):
 	return data
 
 def split_string_to_messages(string):
+	global dateseprarator
+	regex_matcher = '^\\d{2}.\\d{2}.\\d{2}, \\d{2}:\\d{2} - '
+	if('/' in string[:8]):
+		dateformat = 'US'
+		dateseprarator = '/'
+		regex_matcher = '^\\d{2}/\\d{2}/\\d{4}, \\d{2}:\\d{2} - '
+		print('detected mm.dd.yy dateformat (using dateseprarator: ' + dateseprarator + ')')
+	else:
+		print('using dd.mm.yy dateformat')
 	messages = []
 	for line in string.split('\n'):
-		if re.match('^\\d{2}.\\d{2}.\\d{2}, \\d{2}:\\d{2} - ', line):
+		if re.match(regex_matcher, line):
 			messages.append(line)
 		else:
 			messages[-1] += ("\n" + line)
@@ -64,15 +76,20 @@ def to_telegram_format(messages):
 		try:
 			date_time = message.split('-')[0].replace(' ','')
 			date_ = date_time.split(',')[0]
-			date_y = 2000 + int(date_.split('.')[-1])
-			date_m = int(date_.split('.')[1])
-			date_d = int(date_.split('.')[0])
+			if(dateseprarator == '.'):
+				date_y = 2000 + int(date_.split('.')[-1])
+				date_m = int(date_.split('.')[1])
+				date_d = int(date_.split('.')[0])
+			else:
+				date_y = int(date_.split('/')[-1])
+				date_m = int(date_.split('/')[1])
+				date_d = int(date_.split('/')[0])
 			time_ = date_time.split(',')[1]
 			time_h = time_.split(':')[0]
 			time_m = time_.split(':')[1]
 			name = message.split('- ')[1].split(':')[0]
 			text = message.split('- ', 1)[1].split(': ', 1)[1]
-		except:
+		except Exception as e:
 			name = 'automatic'
 			text = ''
 		if('<medi' in text.lower()):
